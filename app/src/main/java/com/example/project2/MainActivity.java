a6c3396d2fb2902a50c607144e5d1f711a4ea1ce
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-04-29 18:47:50 -0700

    fix: make it be able to use ActivityMainBinding in MainActivity

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Entity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.project2.databinding.ActivityMainBinding;
+
+
+//@Entity(tableName = AppData)
 public class MainActivity extends AppCompatActivity {
+    TextView mMainDisplay; // first letter m is for non public and non static field
+    Button mLoginButton;
+    Button mCreateAccountButton;
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
