d6ee4a04dce07d09ab12187f184042fdb335063d
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-05-01 16:28:03 -0700

    add: connect Login to Landing activity

--- app/src/main/java/com/example/project2/LoginActivity.java
+++ app/src/main/java/com/example/project2/LoginActivity.java
@@ -2,13 +2,40 @@ package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.project2.databinding.ActivityLoginBinding;
+import com.example.project2.databinding.ActivityMainBinding;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText mUsernameEditText;
+    EditText mPasswordEditText;
+    Button mLoginButton;
+
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mUsernameEditText = binding.loginUsername;
+        mPasswordEditText = binding.loginPassword;
+        mLoginButton = binding.loginButton;
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplication(), LandingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
