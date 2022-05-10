3f64607fb376a109e805e6bf176b99b3552f847a
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-05-10 02:36:00 -0700

    add: fragments

---
+++ app/src/main/java/com/example/project2/fragment/UsersFragment.java
@@ -0,0 +1,54 @@
+package com.example.project2.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.project2.R;
+
+///**
+// * A simple {@link Fragment} subclass.
+// * Use the {@link UsersFragment#newInstance} factory method to
+// * create an instance of this fragment.
+// */
+public class UsersFragment extends Fragment {
+    public UsersFragment() {
+        // Required empty public constructor
+    }
+
+//    /**
+//     * Use this factory method to create a new instance of
+//     * this fragment using the provided parameters.
+//     *
+//     * @param param1 Parameter 1.
+//     * @param param2 Parameter 2.
+//     * @return A new instance of fragment UsersFragment.
+//     */
+//    // TODO: Rename and change types and number of parameters
+//    public static UsersFragment newInstance(String param1, String param2) {
+//        UsersFragment fragment = new UsersFragment();
+//        Bundle args = new Bundle();
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+//    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_users, container, false);
+    }
+}
