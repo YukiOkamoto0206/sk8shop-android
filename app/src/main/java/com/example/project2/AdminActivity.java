921fcf5e7fae64812cbeee8d92f0527275ead099
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-05-10 01:52:53 -0700

    add: AdminActivity

---
+++ app/src/main/java/com/example/project2/AdminActivity.java
@@ -0,0 +1,64 @@
+package com.example.project2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.project2.ui.main.SectionsPagerAdapter;
+import com.example.project2.databinding.ActivityAdminBinding;
+
+/**
+ * AdminActivity contains whole Fragment(USERS and ITEMS)
+ * tabs -> Tab(USERS / ITEMS)
+ * viewPager -> view pages when the tab is USERS or ITEMS
+ */
+
+/**
+ * AdminActivity > SectionPagerAdapter > PlaceholderFragment > PageViewModel
+ */
+
+public class AdminActivity extends AppCompatActivity {
+
+    private ActivityAdminBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityAdminBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Defined there are two Tabs in this Activity
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public static Intent intentFactory(Context context) {
+        Intent intent = new Intent(context, AdminActivity.class);
+        return intent;
+    }
+}
