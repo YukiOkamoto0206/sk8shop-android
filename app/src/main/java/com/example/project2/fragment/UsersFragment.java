ef41dc40ada3f3426042dbc5f8f594d2e7b2d67b
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-05-10 22:34:33 -0700

    add: delete user method

--- app/src/main/java/com/example/project2/fragment/UsersFragment.java
+++ app/src/main/java/com/example/project2/fragment/UsersFragment.java
@@ -6,22 +6,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.project2.DB.Database;
 import com.example.project2.DB.UserDAO;
 import com.example.project2.R;
 import com.example.project2.User;
-import com.example.project2.databinding.FragmentUsersBinding;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class UsersFragment extends Fragment {
     ListView mListView;
@@ -47,8 +45,24 @@ public class UsersFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         mListView = (ListView) view.findViewById(R.id.userFragmentDisplay);
         mUserDAO = Database.getDatabase(getContext());
-        mArrayList = (ArrayList<User>) mUserDAO.getAllUsers();
-        userDisplay();
+
+        refreshPage();
+
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                User clickedUser = mArrayList.get(i);
+                int userId = getArguments().getInt("USER_ID_KEY_FROM_ADMIN_ACTIVITY");
+                if (clickedUser.getUserId() == userId) {
+                    Toast.makeText(getContext(), "Cannot delete yourself", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+                mUserDAO.deleteUser(clickedUser);
+                Toast.makeText(getContext(), clickedUser.getUsername() + " Deleted", Toast.LENGTH_SHORT).show();
+                refreshPage();
+                return true;
+            }
+        });
     }
 
     private void userDisplay() {
@@ -56,5 +70,10 @@ public class UsersFragment extends Fragment {
             ArrayAdapter arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, mArrayList);
             mListView.setAdapter(arrayAdapter);
         }
+    }
+
+    private void refreshPage() {
+        mArrayList = (ArrayList<User>) mUserDAO.getAllUsers();
+        userDisplay();
     }
 }
