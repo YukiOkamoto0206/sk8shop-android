5e6ab9b4ca1bfa10cb398987dc70ca785214a8ab
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-05-14 01:35:56 -0700

    del: icon

--- app/src/main/java/com/example/project2/AdminActivity.java
+++ app/src/main/java/com/example/project2/AdminActivity.java
@@ -50,15 +50,6 @@ public class AdminActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = binding.fab;
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     public static Intent intentFactory(Context context, int userId) {
